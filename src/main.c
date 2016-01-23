a631ade69dbc595a7d25ac255598034878daa7dd
Elijah Insua <tmpvar@gmail.com>
2016-01-23 18:44:39 -0500

    remove deprecated glu get error method

--- src/main.c
+++ src/main.c
@@ -57,7 +57,32 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 GLint gl_error() {
   GLint error = glGetError();
-  error && printf("ERROR: %s, %i\n", (char *)gluErrorString(error), error);
+
+  switch (error) {
+    case GL_INVALID_ENUM:
+      printf("error (%i): GL_INVALID_ENUM: An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.\n", error);
+    break;
+
+    case GL_INVALID_VALUE:
+      printf("error (%i): GL_INVALID_VALUE: A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.\n", error);
+    break;
+
+    case GL_INVALID_OPERATION:
+      printf("error (%i): GL_INVALID_OPERATION: The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.\n", error);
+    break;
+
+    case GL_STACK_OVERFLOW:
+      printf("error (%i): GL_STACK_OVERFLOW: This command would cause a stack overflow. The offending command is ignored and has no other side effect than to set the error flag.\n", error);
+    break;
+
+    case GL_STACK_UNDERFLOW:
+      printf("error (%i): GL_STACK_UNDERFLOW: This command would cause a stack underflow. The offending command is ignored and has no other side effect than to set the error flag.\n", error);
+    break;
+
+    case GL_OUT_OF_MEMORY:
+      printf("error (%i): GL_OUT_OF_MEMORY: There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.\n", error);
+    break;
+  }
   return error;
 }
 
